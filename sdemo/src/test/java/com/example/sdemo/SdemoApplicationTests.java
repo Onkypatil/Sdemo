30d610839a1d3ca3dde76af9d3be4210dda74c40
Onkar_Kharosekar <75060788+Onkypatil@users.noreply.github.com>
2021-10-17 17:07:07 +0530

    Add files via upload

---
+++ sdemo/src/test/java/com/example/sdemo/SdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
